236730f27e7c12133833da1d36f0341d3e7d4988
Noskine <noskine@outlook.com>
2024-06-20 16:59:16 -0300

    feat:[main] implementation of server test routes

---
+++ pkg/controllers/ConnectionTest.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type ConnectionTestController struct{}
+
+func (h *ConnectionTestController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+		case http.MethodGet:
+			h.Get(w, r)
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprint(w, "Method Not Allowed")
+	}
+}
+
+func (h *ConnectionTestController) Get(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("internal/views/index.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "Internal Server Error")
+	}
+	tmpl.Execute(w, nil)
+}
