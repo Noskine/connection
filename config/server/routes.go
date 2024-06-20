994dc7813cb90d909f36d6b9e85c765a90a7f1cb
Noskine <noskine@outlook.com>
2024-06-20 17:04:15 -0300

    bug:[main] adding function call that displays the view

--- config/server/routes.go
+++ config/server/routes.go
@@ -3,10 +3,14 @@ package server
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/Noskine/connection/pkg/controllers"
 )
 
 func (mux *HttpI) router() {
 	mux.mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Olá mundo!")
 	})
+
+	mux.mux.Handle("/helloworld", &controllers.ConnectionTestController{})
 }
